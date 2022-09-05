b20867a8f493e2b123c1d1d5cc0d6e00e4105a9a
NailtonSSantos <nltsilva8@gmail.com>
2022-09-04 21:54:06 -0400

    lista de paises

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,52 +8,20 @@ router.get('/', (req, res) => {
     res.json({message:'Home'});
 })
 
-const country = ['uruguay']
-
 router.get('/buscaAPI', async  (req, res) => {
-    try {
-            const { data } = await api.get(`http://universities.hipolabs.com/search?country=${country}`);
+    try {   
+            const countries = ["argentina", "brasil", "chile", "colombia", "paraguai", "peru", "suriname", "uruguay"]
+            
+            for(let i=0; i < countries.length; i++){
+
+            let { data } = await api.get(`http://universities.hipolabs.com/search?country=${countries[i]}`);
             res.json({ data })
 
-            try {
-                
-                for (let i = 0; i < data.length; i++){
-                                         
-                    let dadosAPI = []
-
-                    dadosAPI = ({
-                        domains: data.domains,
-                        country: data.country,
-                        alpha_two_code: data.alpha_two_code,
-                        country: data.country,
-                        web_pages: data.web_pages,
-                        name: data.name,
-                        state_province: data.state_province
-                    })
-
-                    async function postAPI(req, res){
-    
-                        const universidade = new UniversidadesModel({
-                            domains,
-                            alpha_two_code,
-                            country,
-                            web_pages,
-                            name,
-                            state_province,
-                        })
-                    
-                        universidade.save()
-                        console.log({message: 'sucesso'})
-                    }
-                }
-            }catch (error) {
-                console.error({ error: error.message})
-            }
+            console.log(countries[i])
+            } 
     } catch (error) {
         res.send({ error: error.message})
     }
-    
-    
 })
 
 router.get('/universidades/:id?', UniversidadeController.get)
